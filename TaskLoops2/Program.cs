bade44c9c402548933087549417c29126bcf8975
ElshanAli <elshanml@code.edu.az>
2022-04-14 14:56:00 +0400

    Added second task

---
+++ TaskLoops2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TaskLoops2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the numbers");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            while (n != 0) 
+            {
+
+                sum = sum + n % 10;
+
+                n = n / 10;
+            }
+
+            Console.WriteLine("Sum of numbers are {0}",sum);
+        }
+    }
+}
